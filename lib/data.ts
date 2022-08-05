84b7f1acd628143f306ff49e02afdedbece41ac8
Yoji Shidara <dara@shidara.net>
2022-08-05 15:54:17 +0900

    Add ITA-corpus and transcription view

---
+++ lib/data.ts
@@ -0,0 +1,52 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+export type Utterance = {
+  id: string;
+  number: number;
+  text: string;
+  reading: string;
+  html: string;
+};
+
+function toRubyHtml(str: string): string {
+  return str.replaceAll(
+    /([\u4E00-\u9FFF々]+)\(([ぁ-んー]+)\)/g,
+    "<ruby><rb>$1</rb><rt>$2</rt></ruby>"
+  );
+}
+
+async function load(filePath: string): Promise<Utterance[]> {
+  const fileContents = await fs.readFile(filePath, "utf8");
+  const lines = fileContents.split("\n");
+
+  const data: Utterance[] = [];
+  for (let line of lines) {
+    const m = line.match(/((^[^_]+)_([^:]+)):([^,]+),(.+)$/);
+    if (m) {
+      data.push({
+        number: Number(m[3]),
+        id: m[1],
+        text: m[4],
+        reading: m[5],
+        html: toRubyHtml(m[4]),
+      });
+    }
+  }
+  return data;
+}
+
+export const getData: () => Promise<Utterance[]> = async () => {
+  const files = [
+    "ita-corpus/emotion_transcript_ruby_utf8.txt",
+    "ita-corpus/recitation_transcript_ruby_utf8.txt",
+  ];
+
+  let data: Utterance[] = [];
+  for (let fn of files) {
+    const filePath = path.join(process.cwd(), "data", fn);
+    data = data.concat(await load(filePath));
+  }
+
+  return data;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,74 @@
-import type { NextPage } from "next";
+import type { InferGetStaticPropsType, NextPage } from "next";
+import { useState } from "react";
 import { Header } from "../components/Header";
+import { getData } from "../lib/data";
 
-const Home: NextPage = () => {
-  return <Header></Header>;
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      utterances: await getData(),
+    },
+  };
+};
+
+const Home: NextPage<Props> = ({ utterances }) => {
+  const [cursor, setCursor] = useState(0);
+
+  const utt = utterances[cursor];
+
+  const handleDelta = (delta: number) => {
+    return () => {
+      setCursor((cursor) =>
+        Math.min(Math.max(cursor + delta, 0), utterances.length - 1)
+      );
+    };
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      <Header />
+
+      <main className="flex-grow">
+        <div className="container mx-auto my-3 px-2">
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body">
+              <div className="mb-2 badge font-bold">{utt.id}</div>
+              <p
+                className="text-2xl leading-10"
+                dangerouslySetInnerHTML={{ __html: utt.html }}
+              />{" "}
+              <p className="text-sm text-gray-500">{utt.reading}</p>
+            </div>
+          </div>
+
+          <div className="my-5">
+            <p className="italic text-right text-gray-500 underline">
+              <a href="https://github.com/mmorise/ita-corpus">ITAコーパス</a>
+            </p>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <div className="mt-5 btn-group grid grid-cols-2">
+          <button
+            className="btn btn-primary rounded-none"
+            onClick={handleDelta(-1)}
+          >
+            &laquo; Prev
+          </button>
+          <button
+            className="btn btn-primary rounded-none"
+            onClick={handleDelta(1)}
+          >
+            Next &raquo;
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
